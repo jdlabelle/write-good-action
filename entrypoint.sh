66e373f96efc3185b1fafd0616eeac2ed7ee08bb
Joshua LaBelle <labelljd@gmail.com>
2025-03-16 22:35:37 -0400

    testing entrypoint.sh by listing filename

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "prior to running write-good..."
 MD_FILES=$(find . -type f -name "*.md")
 for file in "${MD_FILES[@]}"; do
+    echo "linting $file"
     write-good "$file"
 done
-echo "Write-Good exited with $?"
 
 #suggestions=$(write-good "$1")
 #echo "suggestions=$suggestions" >> "$GITHUB_OUTPUT"
