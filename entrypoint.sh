b81c88ac5c2549a12d94fba1c559daae5098438a
Joshua LaBelle <labelljd@gmail.com>
2025-03-16 22:21:31 -0400

    removed inputs in action and fixed script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-suggestions=$(write-good "$1")
-echo "suggestions=$suggestions" >> "$GITHUB_OUTPUT"
+MD_FILES=$(find . -type f -name "*.md")
+for file in "${MD_FILES[@]}"; do
+    write-good "$file"
+done
+
+#suggestions=$(write-good "$1")
+#echo "suggestions=$suggestions" >> "$GITHUB_OUTPUT"
