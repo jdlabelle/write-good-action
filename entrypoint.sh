db6327327221be2840262b516e961ed4fbf4e8c2
Joshua LaBelle <labelljd@gmail.com>
2025-03-18 22:42:58 -0400

    updated conditional and debug output for script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 MD_FILES=$(find . -type f -name "*.md")
 
 # DEBUG: Check the arguments that are being passed
-echo "Arguments passed:"
+echo "Number of Arguments passed: $#"
 for arg in "$@"; do
     echo "$arg"
 done
 
 # Check to see of an optional argument was passed as input
-if [[ $# -eq 1 ]]; then
+if [[ -n $1 ]]; then
     echo "Checks exclude argument supplied"
     INPUT_EXCLUDE_CHECKS=$1
     for file in "${MD_FILES[@]}"; do
