57951c4a08e8d614d579bde69ff4d7d8c50a954a
Joshua LaBelle <labelljd@gmail.com>
2025-03-18 21:49:49 -0400

    add input args to script and add test file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 MD_FILES=$(find . -type f -name "*.md")
-for file in "${MD_FILES[@]}"; do
-    echo "linting $file"
-    write-good "$file"
-done
+
+# Check to see of an optional argument was passed as input
+if [[ $# -eq 1 ]]; then
+    echo "Checks exclude argument supplied"
+    INPUT_EXCLUDE_CHECKS=$1
+    for file in "${MD_FILES[@]}"; do
+        echo "linting $file"
+        write-good "$file" "$INPUT_EXCLUDE_CHECKS"
+    done
+else
+    for file in "${MD_FILES[@]}"; do
+        echo "linting $file"
+        write-good "$file"
+    done
+fi
