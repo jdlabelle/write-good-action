f16d914aa20cb03314d69d5775e95092f5e9e608
Joshua LaBelle <labelljd@gmail.com>
2025-04-29 21:52:19 -0400

    add tex search to regex and include test file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Find all MD files and read them into an array called MD_FILES
+# Find all '.md' and '.tex' files and read them into an array called FILES
 # -t removes trailing newlines. readarray is the same as the "mapfile" command.
-readarray -t MD_FILES < <(find . -type f -name "*.md")
+# readarray -t MD_FILES < <(find . -type f -name "*.md")
+readarray -t FILES < <(find . -type f -regextype posix-extended -regex '.*\.(md|tex)')
 
 # DEBUG: Check the arguments that are being passed
 echo "Number of Arguments passed: $#"
@@ -14,12 +15,14 @@ done
 if [[ -n $1 ]]; then
     echo "Checks argument supplied"
     INPUT_CHECKS=$1
-    for file in "${MD_FILES[@]}"; do
+    #for file in "${MD_FILES[@]}"; do
+    for file in "${FILES[@]}"; do
         echo "linting $file"
         write-good "$file" "$INPUT_CHECKS"
     done
 else
-    for file in "${MD_FILES[@]}"; do
+    #for file in "${MD_FILES[@]}"; do
+    for file in "${FILES[@]}"; do
         echo "linting $file"
         write-good "$file"
     done
